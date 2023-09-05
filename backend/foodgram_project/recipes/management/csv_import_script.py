335d37f36414a19a0745bfe722452c46a78103bd
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-05 18:20:21 +0300

    csv

--- backend/foodgram_project/recipes/management/csv_import_script.py
+++ backend/foodgram_project/recipes/management/csv_import_script.py
@@ -1,19 +1,22 @@
 import csv
-import settings
 
+import settings
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     help = 'Transserfing from csv to database'
+
     def handle(self, *args, **options):
         for model, file in Ingredient.items():
-        with open(settings.BASE_DIR/data/ingredients, encoding='utf-8') as file:
-                if csv_file != 'users.csv':
-                    reader = csv.reader(file)
-                    for row in reader:
-                        Ingredient.objects.create(
-                            name=row['name'],
-                            units_of_measurement=row['units_of_measurement'],)
-
+            with open(
+                    f'{settings.BASE_DIR}/data/{file}',
+                    encoding='utf-8',
+            ) as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    Ingredient.objects.create(
+                        name=row['name'],
+                        units_of_measurement=row['units_of'
+                                                 '_measurement'],)
