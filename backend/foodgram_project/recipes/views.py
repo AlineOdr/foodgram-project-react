2b298a3b50d55e0a606826b0657ddda9a59654ba
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-07 11:36:28 +0300

    recipe2

--- backend/foodgram_project/recipes/views.py
+++ backend/foodgram_project/recipes/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Импортируем модель, чтобы обратиться к ней
 from .models import Recipe
 
+
 def index(request):
     recipes = Recipe.objects.order_by('-pub_date')[:10]
     context = {
