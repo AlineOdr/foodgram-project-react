704a2c8b92b22d29927ba78c3f56f8c7fb4fecde
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-14 18:40:38 +0300

    follow

--- backend/foodgram_project/api/urls.py
+++ backend/foodgram_project/api/urls.py
@@ -2,14 +2,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 # from rest_framework.authtoken import views
-from .views import (CustomUserViewSet, FavoriteViewSet, FollowViewSet,
+from .views import (UserViewSet, FavoriteViewSet, FollowViewSet,
                     IngredientViewSet, RecipeViewSet, ShoppingCartViewSet,
                     TagViewSet)
 
 app_name = 'api'
 
 router = DefaultRouter()
-router.register(r'users', CustomUserViewSet, basename='users')
+router.register(r'users', UserViewSet, basename='users')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
