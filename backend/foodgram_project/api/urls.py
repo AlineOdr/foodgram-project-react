c44851324a9180f47f3f8cac5c64738b27bba9a6
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-14 10:24:08 +0300

    view3

--- backend/foodgram_project/api/urls.py
+++ backend/foodgram_project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 # from rest_framework.authtoken import views
-from .views import (CustomUserViewSet, FavoriteViewSet, FollowViewSet,
-                    IngredientViewSet, RecipeViewSet, ShoppingCartViewSet,
-                    TagViewSet, UserViewSet)
+from .views import (CustomUserViewSet, FavoriteViewSet, IngredientViewSet,
+                    RecipeViewSet, ShoppingCartViewSet, TagViewSet,
+                    UserViewSet)
 
 app_name = 'api'
 
