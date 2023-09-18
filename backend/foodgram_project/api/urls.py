a70eea952803d38df9f239ae76caab58cde1828f
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-18 18:19:58 +0300

    favorrr1

--- backend/foodgram_project/api/urls.py
+++ backend/foodgram_project/api/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
      #     FollowViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
      #     name='subscribe'),
      path('auth/', include('djoser.urls.authtoken')),
-     path('', include(router.urls)),
-#     path('', include('djoser.urls'))
+     path('', include(router.urls))
+     #     path('', include('djoser.urls'))
 ]
