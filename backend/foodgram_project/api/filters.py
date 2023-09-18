5259e0ad9cd79224a5516339c6b2b54b90180280
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-18 18:24:36 +0300

    favorrr2

--- backend/foodgram_project/api/filters.py
+++ backend/foodgram_project/api/filters.py
@@ -9,7 +9,8 @@ class RecipeFilter(filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = "__all__"
+        fields = ("author",
+                  "is_favorited")
 
     def get_favorite(self, queryset, value, name):
         if value:
