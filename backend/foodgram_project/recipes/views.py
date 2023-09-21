577cbc6b3c0d3dc8b307cfcc9a14da7d95c77b5b
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-21 09:22:48 +0300

    delete view def index

--- backend/foodgram_project/recipes/views.py
+++ backend/foodgram_project/recipes/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+#    from django.shortcuts import render
 
 # Импортируем модель, чтобы обратиться к ней
-from .models import Recipe
+#    from .models import Recipe
 
 
-def index(request):
-    recipes = Recipe.objects.order_by('-pub_date')[:10]
-    context = {
-        'recipes': recipes,
-    }
-    return render(request, 'recipes/index.html', context)
+#    def index(request):
+#    recipes = Recipe.objects.order_by('-pub_date')[:10]
+#    context = {
+#        'recipes': recipes,
+#    }
+#    return render(request, 'recipes/index.html', context)
