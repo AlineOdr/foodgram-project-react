9775a446b1f95edbc51bc5d20fb335bf65ea046e
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-21 15:22:05 +0300

    fields ingredients/tag

--- backend/foodgram_project/api/serializers.py
+++ backend/foodgram_project/api/serializers.py
@@ -21,10 +21,10 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
     def get_is_subscribed(self, obj):
-        user = self.context.get('request').user.id
-        if user.is_anonymous:
+        request = self.context.get('request')
+        if request.user.is_anonymous:
             return False
-        return Follow.objects.filter(user=user, author=obj).exists()
+        return Follow.objects.filter(user=request.user, author=obj).exists()
 
     def create(self, validated_data):
         """Создание нового пользователя"""
