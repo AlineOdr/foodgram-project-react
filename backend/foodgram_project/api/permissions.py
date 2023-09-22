7237705c080acd6739fed1afdd24f74763cd617c
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-22 14:07:39 +0300

    permissions1

--- backend/foodgram_project/api/permissions.py
+++ backend/foodgram_project/api/permissions.py
@@ -4,7 +4,8 @@ from rest_framework import permissions
 class AuthorAdminOrAuthenticatedReadOnly(permissions.BasePermission):
     """Разрешения для админа, автора."""
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                or request.user.is_admin
-                or obj.author == request.user)
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated
+            and request.user.is_active
+            and (request.user == obj.author or request.user.is_admin)
+        )
