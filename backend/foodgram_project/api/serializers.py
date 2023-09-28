e3073726ced1932c4dbbfc2af1da07aaebb5e408
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-28 14:20:37 +0300

    update4

--- backend/foodgram_project/api/serializers.py
+++ backend/foodgram_project/api/serializers.py
@@ -223,12 +223,14 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = (
-            "ingredients",
+            "id",
             "tags",
-            "pub_date",
             "author",
-            "image",
+            "ingredients",
+            "is_favorited",
+            "is_in_shopping_cart",
             "name",
+            "image",
             "text",
             "cooking_time"
         )
