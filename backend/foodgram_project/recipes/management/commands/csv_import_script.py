ee68f8eeb26ee032ff5e10eb6fc7fc888bf3752c
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-09-28 11:16:36 +0300

    createrec

--- backend/foodgram_project/recipes/management/commands/csv_import_script.py
+++ backend/foodgram_project/recipes/management/commands/csv_import_script.py
@@ -3,7 +3,7 @@ import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient
 
 
 def read_ingredients():
@@ -17,20 +17,7 @@ def read_ingredients():
             )
 
 
-def read_tags():
-    with open(os.path.join(settings.BASE_DIR, 'data', 'tags.json'),
-              'r', encoding='utf-8') as f:
-        data = json.load(f)
-        for i in range(len(data)):
-            Tag.objects.get_or_create(
-                name=data[i].get("name"),
-                slug=data[i].get("slug"),
-                color=data[i].get("color")
-            )
-
-
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         read_ingredients()
-        #        read_tags()
