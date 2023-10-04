24e01b52934633a785c1c47214f30248cade3b3a
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-10-04 16:44:16 +0300

    taggg!

--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -1,9 +1,6 @@
 from colorfield.fields import ColorField
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import (
-    MaxValueValidator,
-    MinValueValidator,
-)
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
